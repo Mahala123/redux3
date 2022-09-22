947efd3bf538d75b7a6856a59eca75e89c2a0fa9
sambit <sambitmahala653@gmail.com>
2022-09-22 16:20:13 +0530

    modify slice

--- src/components/Store/UI-slice.js
+++ src/components/Store/UI-slice.js
@@ -2,10 +2,18 @@ import{createSlice} from '@reduxjs/toolkit';
 
 const UiSlice=createSlice({
     name:'UI',
-    initialState:{carIsVisible:false},
+    initialState:{carIsVisible:false,notification:null},
     reducers:{
         toggle(state){
             state.carIsVisible=!state.carIsVisible
+        },
+        showNotification(state,action)
+        {
+            state.notification={
+                status:action.payload.stats,
+                title:action.payload.title,
+                message:action.payload.message
+            }
         }
     }
 })
